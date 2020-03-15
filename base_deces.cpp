09d320e8f782cc518b0b58275471f7b94c0ea333
Luka <luka.pavageau@gmail.com>
2020-03-15 17:59:32 +0100

    simple tree working

--- base_deces.cpp
+++ base_deces.cpp
@@ -10,77 +10,73 @@
 
  // using namespace std;
 
-void lire_base_deces(const std::string& nomfic, base_deces_t& bp)
-{
-	try {
-		deces_file_t f(nomfic);
-		f.read_header(io::ignore_extra_column,"prenom","sexe");
-		std::string prenomstr;
-		int sexe;
-		std::string p_sep, prenom;
-		// uint32_t annee, nombre;
-		while (f.read_row(prenomstr, sexe)) {
-      int sexe_a = sexe;
+ void lire_base_deces(const std::string& nomfic, base_deces_t& bp)
+ {
+       try {
+             deces_file_t f(nomfic);
+             f.read_header(io::ignore_extra_column,"prenom");
+             std::string prenomstr;
+             std::string p_sep, prenom;
+             // uint32_t annee, nombre;
+             while (f.read_row(prenomstr)) {
 
-			//séparation des prénoms
-      std::vector<prenom_t> liste_prenoms;
-      separate(prenomstr, liste_prenoms, sexe_a);
-      std::cout << "Liste de prénoms créée" << '\n';
+                   //séparation des prénoms
+                   std::vector<std::string> liste_prenoms;
+                   separate(prenomstr, liste_prenoms);
+                   std::cout << "Liste de prénoms créée" << '\n';
 
-      prenom_add(liste_prenoms, bp);
-      for (prenom_t prenom : liste_prenoms) {
-        std::cout << prenom << '\n';
+                   prenom_add(liste_prenoms, bp);
+                   for (std::string prenom : liste_prenoms) {
+                        std::cout << prenom << '\n';
+                  }
+                  // stringstream s(prenomstr);
+                  //
+                  // while (s>>p_sep){
+                  //
+                  // 	auto prenom = prenom_t{p_sep,sexe_t(sexe)};
+                  // 	const auto& iter = bp.find(prenom);
+                  // 	if (iter != bp.end()) { // Le prénom est déjà dans la base ?
+                  //
+                  // 		// bp[prenom];
+                  // 		// => on ajoute l'année courante et le nombre d'occurrences
+                  // 	// 	++(iter->second)[annee-1900];
+                  // 	} else { // Le prénom n'est pas dans la base ?
+                  // 	// // => On l'y ajoute
+                  // 		bp[prenom];
+                  // 	}
+                  // }
+
+
+            }
+
+
+      } catch (const io::error::can_not_open_file& e) {
+            std::cerr << e.what() << "\n";
+            exit(1);
       }
-			// stringstream s(prenomstr);
-      //
-			// while (s>>p_sep){
-      //
-			// 	auto prenom = prenom_t{p_sep,sexe_t(sexe)};
-			// 	const auto& iter = bp.find(prenom);
-			// 	if (iter != bp.end()) { // Le prénom est déjà dans la base ?
-      //
-			// 		// bp[prenom];
-			// 		// => on ajoute l'année courante et le nombre d'occurrences
-			// 	// 	++(iter->second)[annee-1900];
-			// 	} else { // Le prénom n'est pas dans la base ?
-			// 	// // => On l'y ajoute
-			// 		bp[prenom];
-			// 	}
-			// }
-
-
-		}
-
-
-	} catch (const io::error::can_not_open_file& e) {
-		std::cerr << e.what() << "\n";
-		exit(1);
-	}
 }
 
 
 //fonction séparant les prénoms associés et en faisant une liste
-void separate(std::string prenoms_str, auto& liste_p, int sexe){
-  int sexe_a = sexe;
-	std::stringstream s(prenoms_str);
-	std::string p_sep;
-	while (s>>p_sep){
-    prenom_t p_sepa = prenom_t{p_sep,sexe_t(sexe_a)};
-    liste_p.push_back(p_sepa);
-	}
+void separate(std::string prenoms_str, auto& liste_p){
+      std::stringstream s(prenoms_str);
+      std::string p_sep;
+      while (s>>p_sep){
+            liste_p.push_back(p_sep);
+      }
 }
 
 // TODO
 //fonction prenant la liste de prénoms associés,
 //recherchant chacun de ces prénoms dans la base_deces
 //et ajoutant les autres prénoms à la table de hash associée
-void prenom_add(std::vector<prenom_t> liste_prenoms, base_deces_t& base_deces){
-      for (prenom_t prenom : liste_prenoms) {
+void prenom_add(std::vector<std::string> liste_prenoms, base_deces_t& base_deces){
+      for (std::string prenom : liste_prenoms) {
             auto iter = base_deces.find(prenom);
 
         	if (iter != base_deces.end()) { // Le prénom est déjà dans la base ?
                   //on ajoute les prénoms associés à la table de hash
-                  for (prenom_t prenom_A : liste_prenoms) {
+                  for (std::string prenom_A : liste_prenoms) {
                         if (prenom != prenom_A){
                               branche_t branche;
                               branche[prenom_A] = 0;
@@ -92,7 +88,7 @@ void prenom_add(std::vector<prenom_t> liste_prenoms, base_deces_t& base_deces){
                   // prenom_t iter = iter;
                   base_deces[prenom];
 
-                  for (prenom_t prenom_A : liste_prenoms) {
+                  for (std::string prenom_A : liste_prenoms) {
                         if (prenom != prenom_A){
                               branche_t branche;
                               branche[prenom_A] = 0;
--- base_deces.hpp
+++ base_deces.hpp
@@ -11,7 +11,7 @@
 #include <array>
 #include <csv/csv.h>
 
-using deces_file_t =  io::CSVReader<2, io::trim_chars<' '>,
+using deces_file_t =  io::CSVReader<1, io::trim_chars<' '>,
 										 io::double_quote_escape<',','\"'> >;
 
 enum class sexe_t {
@@ -35,21 +35,21 @@ struct hash_pair {
 
 struct equality_prenom_t {
 	bool operator()(const prenom_t& p1, const prenom_t& p2) const {
-		return p1.first == p2.first;
+		return p1.first == p2.first && p1.second == p2.second;
 	}
 };
 
 //Une table qui contiendra des prénoms et un poids associé
-using branche_t = std::unordered_map<prenom_t,int, hash_pair,equality_prenom_t >;
+using branche_t = std::unordered_map<std::string,int>;
 //Une table qui contiendra pour chaque prénom, la table des prénoms associés branche_t
-using base_deces_t = std::unordered_map<prenom_t,branche_t,hash_pair,equality_prenom_t>;
+using base_deces_t = std::unordered_map<std::string,branche_t>;
 
 //fonction lisant le fichier et englobant les autres fonctions
 void lire_base_deces(const std::string& nomfic, base_deces_t& bp);
 //fonction séparant les prénoms associés et renvoyant une paire prenom/prénoms associés
-void separate(std::string prenoms_str, auto& liste_p, int sexe);
+void separate(std::string prenoms_str, auto& liste_p);
 //fonction insérant ces prénoms dans la base
-void prenom_add(std::vector<prenom_t> liste_prenoms, base_deces_t& base_deces);
+void prenom_add(std::vector<std::string> liste_prenoms, base_deces_t& base_deces);
 
 
 
